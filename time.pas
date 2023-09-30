0f3b5ec3573f8b033069b437e9dbfa706da2a70e
Максим Трутнев <justinfoley99@mail.ru>
2023-09-30 10:45:07 +0300

    Добавил прогу проверящую году на високосность.

---
+++ time.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var god:=ReadInteger('Введите год , а я проверю его на високосность:');
+  if (god mod 4=0)or((god mod 100=0)and(god mod 400<>0)) then
+    Println($'{god} год - високосный.')
+  else Println($'{god} год - невисокосный.')
+end.
