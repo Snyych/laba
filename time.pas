36c88e900ffdf52c3cbf283e3d70c86cd83af62b
Максим Трутнев <justinfoley99@mail.ru>
2023-09-30 10:51:37 +0300

    Исправил прогу , теперь выводится количество дней в вводимом году.

--- time.pas
+++ time.pas
@@ -1,6 +1,6 @@
 ﻿begin
   var god:=ReadInteger('Введите год , а я проверю его на високосность:');
-  if (god mod 4=0)or((god mod 100=0)and(god mod 400<>0)) then
-    Println($'{god} год - високосный.')
-  else Println($'{god} год - невисокосный.')
+  if (god mod 4=0)and((god mod 100<>0)or(god mod 400=0)) then
+    Println($'В {god} году 366 дней.')
+  else Println($'В {god} году 365 дней.')
 end.
