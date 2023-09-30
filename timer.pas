d4631b1e0cf0e8daadfe90aedef81329c937c2e2
Максим Трутнев <justinfoley99@mail.ru>
2023-09-30 10:53:28 +0300

    Отформатировал все программы , написал программу которая переводит часы , минуты и секунды в секунды.

---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿//timer.pas
+begin
+  var (ch, min, sec) := ReadInteger3('Введите количество часов , минут и секунд');
+  var symm := ch * 3600 + min * 60 + sec;
+  Println($'Итого : {symm} секунд');
+end.
